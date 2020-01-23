1529a281eba7ef99449bc804d1b41617f6db49d5
BernardoMB <bmondragonbrozon@gmail.com>
2020-01-23 11:35:24 -0600

    Fixed Azure Media Player video tag outline style

--- src/app/modules/courses/components/amp-player/amp-player.component.ts
+++ src/app/modules/courses/components/amp-player/amp-player.component.ts
@@ -43,6 +43,21 @@ export class AmpPlayerComponent implements OnInit {
           ],
         },
       },
+      playbackSpeed: {
+        enabled: true,
+        initialSpeed: 1.0,
+        speedLevels: [
+          { name: 'x4.0', value: 4.0 },
+          { name: 'x3.0', value: 3.0 },
+          { name: 'x2.0', value: 2.0 },
+          { name: 'x1.75', value: 1.75 },
+          { name: 'x1.5', value: 1.5 },
+          { name: 'x1.25', value: 1.25 },
+          { name: 'normal', value: 1.0 },
+          { name: 'x0.75', value: 0.75 },
+          { name: 'x0.5', value: 0.5 },
+        ],
+      },
     });
 
     myPlayer.ngComponent = this;
