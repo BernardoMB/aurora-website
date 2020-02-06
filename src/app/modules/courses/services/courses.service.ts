398becbcbba23913c7f7b9dae5554611d6246572
BernardoMB <bmondragonbrozon@gmail.com>
2020-02-06 12:19:20 -0600

    Fixed landing page

--- src/app/modules/courses/services/courses.service.ts
+++ src/app/modules/courses/services/courses.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { log } from 'util';
 import { Course } from 'src/app/shared/models/course.model';
 import { Category } from 'src/app/shared/models/category.model';
 import { environment } from '../../../../environments/environment';
@@ -15,40 +14,37 @@ export class CoursesService {
 
   constructor(private http: HttpClient) {}
 
-  getFeaturedCourses(
-    skip: number, // for pagination
-    limit: number // for pagination
-  ): Observable<Array<Course>> {
+  getFeaturedCourses(skip: number, limit: number): Observable<Array<Course>> {
     console.log('Coureses service: Getting featured courses');
-    // this.host = `https://mainserver.azurewebsites.net`; // TODO: delete this line
     const url = `${this.host}/${this.apiVersion}/courses/public?skip=${skip}&limit=${limit}&populate=category`;
     return this.http.get<Array<Course>>(url);
   }
 
   getRecentCourses(skip: number, limit: number): Observable<Array<Course>> {
     console.log('Coureses service: Getting recent courses');
-    // this.host = `https://mainserver.azurewebsites.net`; // TODO: delete this line
-    const url = `${this.host}/${this.apiVersion}/courses/public?skip=${skip}&limit=${limit}&populate=category&sort=-createdAt`;
+    let url: string;
+    if (skip && limit) {
+      url = `${this.host}/${this.apiVersion}/courses/public?skip=${skip}&limit=${limit}&populate=category&sort=-createdAt`;
+    } else {
+      url = `${this.host}/${this.apiVersion}/courses/public?populate=category&sort=-createdAt`;
+    }
     return this.http.get<Array<Course>>(url);
   }
 
   getCategories(): Observable<Array<Category>> {
     console.log('Coureses service: Getting categories');
-    // this.host = `https://mainserver.azurewebsites.net`; // TODO: delete this line
     const url = `${this.host}/${this.apiVersion}/categories`;
     return this.http.get<Array<Category>>(url);
   }
 
   getCategory(categoryId: string): Observable<Category> {
     console.log(`Courses service: Getting category with id ${categoryId}`);
-    // this.host = `https://mainserver.azurewebsites.net`; // TODO: delete this line
     const url = `${this.host}/${this.apiVersion}/categories/${categoryId}`;
     return this.http.get<Category>(url);
   }
 
   getCategoryFeaturedCourses(categoryId: string): Observable<Array<Course>> {
     console.log(`Courses service: Getting featured courses of category with id ${categoryId}`);
-    // this.host = `https://mainserver.azurewebsites.net`; // TODO: delete this line
     const url = `${this.host}/${this.apiVersion}/courses/public?category=${categoryId}`;
     return this.http.get<Array<Course>>(url);
   }
@@ -56,21 +52,18 @@ export class CoursesService {
   getCategoryCourses(categoryId: string): Observable<Array<Course>> {
     // TODO: This endpoint must never be called without pagination.
     console.log(`Courses service: Getting featured courses of category with id ${categoryId}`);
-    // this.host = `https://mainserver.azurewebsites.net`; // TODO: delete this line
     const url = `${this.host}/${this.apiVersion}/courses/public?category=${categoryId}`;
     return this.http.get<Array<Course>>(url);
   }
 
   getCourse(courseId: string): Observable<Course> {
     console.log(`Courses service: Getting course with id ${courseId}`);
-    // this.host = `https://mainserver.azurewebsites.net`; // TODO: delete this line
     const url = `${this.host}/${this.apiVersion}/courses/${courseId}?populate=category`;
     return this.http.get<Course>(url);
   }
 
   getCoursesFromIds(courseIds: string[]): Observable<Course[]> {
     console.log('Courses service: Getting courses providing array of ids');
-    // this.host = `https://mainserver.azurewebsites.net`; // TODO: delete this line
     const url = `${this.host}/${this.apiVersion}/courses/courses`;
     return this.http.post<Course[]>(url, {courseIds});
   }
