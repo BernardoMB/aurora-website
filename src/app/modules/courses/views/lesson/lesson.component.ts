3c9b3a6838a7bd0e1a66160d7c423ea43c7b973a
BernardoMB <bmondragonbrozon@gmail.com>
2020-02-18 15:34:52 -0600

    Added download file button

--- src/app/modules/courses/views/lesson/lesson.component.ts
+++ src/app/modules/courses/views/lesson/lesson.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Subscription, Observable } from 'rxjs';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-lesson',
@@ -9,6 +11,7 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./lesson.component.scss'],
 })
 export class LessonComponent implements OnInit {
+  @ViewChild('downloadLink', { static: false }) private downloadLink: ElementRef;
 
   decryptionKeyToken: string;
 
@@ -17,16 +20,29 @@ export class LessonComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private http: HttpClient,
+    private sanitizer: DomSanitizer
   ) {}
 
   ngOnInit() {
     this.decryptionKeyToken = '';
     this.route.data.subscribe((data: any) => {
       if (data.lesson) {
-        console.log('Lesson', data.lesson);
         this.lesson = data.lesson;
       }
     });
+  }
+
+  async downloadFile() {
+    // Best option
+    // const link = this.downloadLink.nativeElement;
+    const link = document.createElement('a');
+    link.href = this.lesson.documentUrl;
+    link.download = this.lesson.title;
+    link.download = 'pene.pdf';
+    link.click();
+    window.URL.revokeObjectURL(this.lesson.documentUrl);
+    window.open(this.lesson.documentUrl);
   }
 }
